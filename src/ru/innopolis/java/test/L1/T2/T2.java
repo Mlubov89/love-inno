d577a37c079e1c9483913f78bfc8d62aa8c0148e
lubovmarfutkina <lt89@bk.ru>
2024-06-08 21:34:21 +0300

    T2

---
+++ src/ru/innopolis/java/test/L1/T2/T2.java
@@ -0,0 +1,48 @@
+package ru.innopolis.java.test.L1.T2;
+
+import java.util.Random;
+
+public class T2 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int number = random.nextInt(3);
+        int number2 = random.nextInt(3);
+        //камень-0,ножницы-1,бумага-2
+
+        System.out.println(number);
+        System.out.println(number2);
+
+        if (number == 0){
+            if (number2 == 1){
+                System.out.println("Вася выиграл");
+            }
+            else if (number2 == 2){
+                System.out.println("Петя выиграл");
+            }
+            else {
+                System.out.println("ничья");
+            }
+        }
+        if (number == 1){
+            if (number2 == 0){
+                System.out.println("Петя выиграл");
+            }
+            else if (number2 == 2){
+                System.out.println("Вася выиграл");
+            }
+            else {
+                System.out.println("ничья");
+            }
+        }
+        if (number == 2){
+            if (number2 == 0){
+                System.out.println("Вася выиграл");
+            } else if (number2 == 1){
+                System.out.println("Петя выиграл");
+            }
+            else {
+                System.out.println("ничья");
+            }
+        }
+    }
+}
