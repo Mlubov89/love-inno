26ba2b655be27748e1297aada576a24cb5f83f0e
lubovmarfutkina <lt89@bk.ru>
2024-08-20 13:47:50 +0300

    L4 T1 T2

---
+++ src/ru/innopolis/java/test/L4/T1/T1.java
@@ -0,0 +1,20 @@
+package ru.innopolis.java.test.L4.T1;
+
+import java.util.Scanner;
+
+public class T1 {
+    public static void main(String[] args) {
+        String keyboard = "qwertyuiopasdfghjklzxcvbnm";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите одинсимвол — маленькую букву английского алфавита: ");
+        String word = scanner.next();
+        int index = keyboard.indexOf(word);
+        if (word.equals("q")) {
+            System.out.println("m");
+        }
+        else {
+            char letter = keyboard.charAt(index - 1);
+            System.out.println(letter);
+        }
+    }
+}
