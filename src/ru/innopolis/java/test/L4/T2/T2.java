08203f31db1f2ec28978289bf0d426d54330f092
lubovmarfutkina <lt89@bk.ru>
2024-10-04 12:25:58 +0300

    T2

---
+++ src/ru/innopolis/java/test/L4/T2/T2.java
@@ -0,0 +1,25 @@
+package ru.innopolis.java.test.L4.T2;
+
+import java.util.Scanner;
+
+public class T2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String symbol = ">>-->";
+        String symbol2 = "<--<<";
+        int lastIndex = 0;
+        int index = 0;
+        while ((lastIndex = input.indexOf(symbol, lastIndex)) != -1) {
+            index++;
+            lastIndex += symbol.length();
+        }
+        while ((lastIndex = input.indexOf(symbol2, lastIndex)) != -1) {
+            index++;
+            lastIndex += symbol2.length();
+        }
+        System.out.println(index);
+    }
+}
+
+// >>-->>>-->>>-->-------<--<<<--<<<--<<<--<<----<<<<>>>> //
